9adb58ebef0fabf76c4f9e8b6a2344f787f7f45a
Yaroslav Kovalsky <yatsektest@gmail.com>
2020-12-15 16:56:12 +0300

    cigarParty is solved

--- CodingBat/src/Logic1.java
+++ CodingBat/src/Logic1.java
@@ -1,12 +1,12 @@
 public class Logic1 {
     public static void main(String[] args) {
 
-
+        System.out.println(cigarParty(70, true));
 
     }
 
     static boolean cigarParty(int cigars, boolean isWeekend) {
-        return true;
+        return (isWeekend && cigars >= 40) || (!isWeekend && cigars >= 40 && cigars <= 60);
     }
 
 }
