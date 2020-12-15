278aa71895903ac1877de8903cdaa91662249a4f
Yaroslav Kovalsky <yatsektest@gmail.com>
2020-12-15 18:13:55 +0300

    squirrelPlay is solved

--- CodingBat/src/Logic1.java
+++ CodingBat/src/Logic1.java
@@ -2,13 +2,18 @@ public class Logic1 {
     public static void main(String[] args) {
 
 //        System.out.println(cigarParty(70, true));
-        System.out.println(dateFashion(2, 9));
+//        System.out.println(dateFashion(2, 9));
+        System.out.println(squirrelPlay(95, true));
 
     }
 
+    static boolean squirrelPlay(int temp, boolean isSummer) {
+        return (!isSummer && temp >= 60 && temp <=90) || (isSummer && temp >= 60 && temp <=100);
+    }
+
     static int dateFashion(int you, int date) {
-        if((you >= 8 && date > 2) || (date >= 8 && you > 2)) return 2; // Неоптимальное решение - можно короче?
-        if((you < 8 && date > 2) && (date < 8 && you > 2)) return 1;
+        if ((you >= 8 && date > 2) || (date >= 8 && you > 2)) return 2; // Неоптимальное решение - можно короче?
+        if ((you < 8 && date > 2) && (date < 8 && you > 2)) return 1;
         return 0;
 /*      // Check the <=2 case first, since it takes precedence    // Решение с сайта
         // over the >=8 case.
