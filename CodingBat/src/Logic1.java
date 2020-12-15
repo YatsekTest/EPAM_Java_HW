3fbe30f24fc2262c77d710bcb2ba914197e39da7
Yaroslav Kovalsky <yatsektest@gmail.com>
2020-12-15 17:36:22 +0300

    dateFashion is solved

--- CodingBat/src/Logic1.java
+++ CodingBat/src/Logic1.java
@@ -1,8 +1,21 @@
 public class Logic1 {
     public static void main(String[] args) {
 
-        System.out.println(cigarParty(70, true));
+//        System.out.println(cigarParty(70, true));
+        System.out.println(dateFashion(2, 9));
 
+    }
+
+    static int dateFashion(int you, int date) {
+        if((you >= 8 && date > 2) || (date >= 8 && you > 2)) return 2; // Неоптимальное решение - можно короче?
+        if((you < 8 && date > 2) && (date < 8 && you > 2)) return 1;
+        return 0;
+/*      // Check the <=2 case first, since it takes precedence    // Решение с сайта
+        // over the >=8 case.
+        if (you <= 2 || date <= 2) return 0;
+        else if (you >= 8 || date >= 8) return 2;
+        else return 1;
+        }   */
     }
 
     static boolean cigarParty(int cigars, boolean isWeekend) {
