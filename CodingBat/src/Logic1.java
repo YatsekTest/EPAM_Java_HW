effa19af508c8f3b2068eb26257e7e6cbdab93e1
Yaroslav Kovalsky <yatsektest@gmail.com>
2020-12-16 14:50:32 +0300

    caughtSpeed is solved

--- CodingBat/src/Logic1.java
+++ CodingBat/src/Logic1.java
@@ -3,12 +3,23 @@ public class Logic1 {
 
 //        System.out.println(cigarParty(70, true));
 //        System.out.println(dateFashion(2, 9));
-        System.out.println(squirrelPlay(95, true));
+//        System.out.println(squirrelPlay(95, true));
+        System.out.println(caughtSpeeding(80, false));
+    }
 
+    static int caughtSpeeding(int speed, boolean isBirthday) {
+        int birthday = 0;
+        if (isBirthday) birthday = 5;
+        if (speed <= 60 + birthday) return 0;
+        else if (speed > 60 + birthday && speed <= 80 + birthday) return 1;
+        else if (speed > 80 + birthday) return 2;
+        return 2;   /* Смущает меня необходимость в конце метода всё равно выводить ещё один return. Наверное можно
+                        как-то оптимизировать код. Хотя, метод не знает, что на вход могут поступить только строго
+                           определённые значения... */
     }
 
     static boolean squirrelPlay(int temp, boolean isSummer) {
-        return (temp >= 60 && temp <=90) || (isSummer && temp >= 60 && temp <=100);
+        return (temp >= 60 && temp <= 90) || (isSummer && temp >= 60 && temp <= 100);
     }
 
     static int dateFashion(int you, int date) {
