6fbc91ab50e111f0f4eba3e777b7bb5b9281d28a
Yaroslav Kovalsky <yatsektest@gmail.com>
2020-12-22 22:29:57 +0300

    has23 is solved

--- CodingBat/src/Array1.java
+++ CodingBat/src/Array1.java
@@ -4,6 +4,10 @@ public class Array1 {
 
     }
 
+    static boolean has23(int[] nums) {
+        return nums[0] == 2 ||  nums[0] == 3 || nums[1] == 2 || nums[1] == 3;
+    }
+
     static int[] makeEnds(int[] nums) {
         return new int[]{nums[0], nums[nums.length - 1]};
     }
