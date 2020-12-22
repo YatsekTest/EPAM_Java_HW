2ad4c87b29ceb12c54373da191d03c7ef9eca714
Yaroslav Kovalsky <yatsektest@gmail.com>
2020-12-22 21:41:00 +0300

    sameFirstLast is solved

--- CodingBat/src/Array1.java
+++ CodingBat/src/Array1.java
@@ -1,7 +1,12 @@
 public class Array1 {
     public static void main(String[] args) {
-        int[] nums = {1, 2, 6};
-        System.out.println(firstLast6(nums));
+//        int[] nums = {1, 2, 6};
+//        System.out.println(firstLast6(nums));
+
+    }
+
+    static boolean sameFirstLast(int[] nums) {
+        return nums.length > 0 && nums[0] == nums[nums.length - 1];
     }
 
     static boolean firstLast6(int[] nums) {
