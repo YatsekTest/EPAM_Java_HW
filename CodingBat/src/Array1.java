90f976992cf9b806934af56024df00c3fc1f16c3
Yaroslav Kovalsky <yatsektest@gmail.com>
2020-12-22 21:46:28 +0300

    makePi is solved

--- CodingBat/src/Array1.java
+++ CodingBat/src/Array1.java
@@ -1,8 +1,11 @@
 public class Array1 {
     public static void main(String[] args) {
-//        int[] nums = {1, 2, 6};
-//        System.out.println(firstLast6(nums));
+//        System.out.println(firstLast6(new int[]{1, 2, 6}));
 
+    }
+
+    static int[] makePi() {
+        return new int[]{3, 1, 4};
     }
 
     static boolean sameFirstLast(int[] nums) {
