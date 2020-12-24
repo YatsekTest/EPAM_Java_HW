21bc8b584ae8e090580c629c9709fe9b1295fee0
Yaroslav Kovalsky <yatsektest@gmail.com>
2020-12-24 19:41:36 +0300

    frontTimes is solved

--- CodingBat/src/Warmup2.java
+++ CodingBat/src/Warmup2.java
@@ -1,6 +1,18 @@
 public class Warmup2 {
     public static void main(String[] args) {
-        System.out.println(stringTimes("Hi", 2));
+//        System.out.println(stringTimes("Hi", 2));
+        System.out.println(frontTimes("Chocolate", 5));
+    }
+
+    static String frontTimes(String str, int n) {
+        String newStr = "";
+        String subStr = "";
+        if(str.length() >= 3) subStr = str.substring(0, 3);
+        else subStr = str;
+        for (int i = 0; i < n; i++) {
+            newStr += subStr;
+        }
+        return newStr;
     }
 
     static String stringTimes(String str, int n) { // Более изящно можно решить со StringBuilder.
