37c382651eb561626004c036fab097229725a6ad
Yaroslav Kovalsky <yatsektest@gmail.com>
2020-12-25 19:34:44 +0300

    array667 is solved

--- CodingBat/src/Warmup2.java
+++ CodingBat/src/Warmup2.java
@@ -11,6 +11,14 @@ public class Warmup2 {
 //        System.out.println(stringX("abxxxcd"));
     }
 
+    static int array667(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if(nums[i] == 6 && (nums[i + 1] == 6 || nums[i + 1] == 7)) count++;
+        }
+        return count;
+    }
+
     static String stringYak(String str) {
         return str.replace("yak", "");
 //        Решение приведённое на сайте:
