d0a407e58c7f725650ad52937ecd8a802977675d
Yaroslav Kovalsky <yatsektest@gmail.com>
2020-12-25 19:47:41 +0300

    noTriples is solution is corrected

--- CodingBat/src/Warmup2.java
+++ CodingBat/src/Warmup2.java
@@ -11,12 +11,15 @@ public class Warmup2 {
 //        System.out.println(stringX("abxxxcd"));
     }
 
+    static boolean has271(int[] nums) {
+
+    }
+
     static boolean noTriples(int[] nums) {
-        int count = 0;
         for (int i = 0; i < nums.length - 2; i++) {
-            if(nums[i] == nums[i + 1] && nums[i] == nums[i + 2]) count++;
+            if(nums[i] == nums[i + 1] && nums[i] == nums[i + 2]) return false;
         }
-        return count == 0;
+        return true;
     }
 
     static int array667(int[] nums) {
