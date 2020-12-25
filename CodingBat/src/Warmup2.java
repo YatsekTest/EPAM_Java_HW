856d2282fcb0c32d34b3dbb732b00e528f613886
Yaroslav Kovalsky <yatsektest@gmail.com>
2020-12-25 11:24:26 +0300

    last2 is solved

--- CodingBat/src/Warmup2.java
+++ CodingBat/src/Warmup2.java
@@ -5,7 +5,23 @@ public class Warmup2 {
 //        System.out.println(countXXX("xxxx"));
 //        System.out.println(doubleX("xaxxx"));
 //        System.out.println(stringBits("Heeololeo"));
-        System.out.println(stringSplosion("Code"));
+//        System.out.println(stringSplosion("Code"));
+    }
+
+    static int last2(String str) {
+        // Screen out too-short string case.
+        if (str.length() < 2) return 0;
+        String end = str.substring(str.length() - 2);
+        // Note: substring() with 1 value goes through the end of the string
+        int count = 0;
+        // Check each substring length 2 starting at i
+        for (int i = 0; i < str.length() - 2; i++) {
+            String sub = str.substring(i, i + 2);
+            if (sub.equals(end)) {  // Use .equals() with strings
+                count++;
+            }
+        }
+        return count;
     }
 
     static String stringSplosion(String str) {
