68b5a4f9deaf046c8896ab40cd379936ce5f92ad
Yaroslav Kovalsky <yatsektest@gmail.com>
2020-12-28 15:40:26 +0300

    centeredAverage is solved

--- CodingBat/src/Array2.java
+++ CodingBat/src/Array2.java
@@ -1,6 +1,18 @@
 public class Array2 {
     public static void main(String[] args) {
-        System.out.println(bigDiff(new int[]{10, 3, 5, 6}));
+//        System.out.println(bigDiff(new int[]{10, 3, 5, 6}));
+    }
+
+    static int centeredAverage(int[] nums) {
+        int largest = nums[0];
+        int smallest = nums[0];
+        int sum = 0;
+        for (int num : nums) {
+            largest = Math.max(largest, num);
+            smallest = Math.min(smallest, num);
+            sum += num;
+        }
+        return (sum - largest - smallest) / (nums.length - 2);
     }
 
     static int bigDiff(int[] nums) {
