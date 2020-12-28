f46cbacc8c308a5d9fcf54e1d20f12101ea0deb2
Yaroslav Kovalsky <yatsektest@gmail.com>
2020-12-28 10:15:16 +0300

    makeBricks is solved

--- CodingBat/src/Logic2.java
+++ CodingBat/src/Logic2.java
@@ -2,4 +2,9 @@ public class Logic2 {
     public static void main(String[] args) {
 
     }
+
+    static boolean makeBricks(int small, int big, int goal) {
+        return goal <= big * 5 + small && goal % 5 <= small;
+    }
+
 }
