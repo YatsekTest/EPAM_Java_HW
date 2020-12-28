7322f1e050d962b4055eea2c2259206747d2d0e9
Yaroslav Kovalsky <yatsektest@gmail.com>
2020-12-28 14:07:33 +0300

    makeChocolate is solved

--- CodingBat/src/Logic2.java
+++ CodingBat/src/Logic2.java
@@ -3,30 +3,36 @@ public class Logic2 {
         System.out.println(19 % 21);
     }
 
+    static int makeChocolate(int small, int big, int goal) {
+        if (goal > (big * 5 + small) || goal % 5 > small) return -1;
+        if (big * 5 < goal) return goal - big * 5;
+        else return goal % 5;
+    }
+
     static boolean evenlySpaced(int a, int b, int c) {
-        return b - a  == c - b || b - a == a - c || a - c == c - b;
+        return b - a == c - b || b - a == a - c || a - c == c - b;
     }
 
     static int blackjack(int a, int b) {
-        if(a > 21 && b > 21) return 0;
-        if(b > 21) return a;
-        if(a > 21) return b;
-        if(21 - a < 21 - b) return a;
+        if (a > 21 && b > 21) return 0;
+        if (b > 21) return a;
+        if (a > 21) return b;
+        if (21 - a < 21 - b) return a;
         else return b;
     }
 
     static boolean closeFar(int a, int b, int c) {
-        if(Math.abs(b - a) <= 1 && Math.abs(c - a) > 1 && Math.abs(c - b) > 1) return true;
+        if (Math.abs(b - a) <= 1 && Math.abs(c - a) > 1 && Math.abs(c - b) > 1) return true;
         return Math.abs(c - a) <= 1 && Math.abs(b - a) > 1 && Math.abs(b - c) > 1;
     }
 
     static int roundSum(int a, int b, int c) {
-       return round10(a) + round10(b) + round10(c);
+        return round10(a) + round10(b) + round10(c);
     }
 
     static int round10(int num) {
-        if(num % 10 >= 5) return num + (10 - num% 10);
-        if(num % 10 < 5) return num - num % 10;
+        if (num % 10 >= 5) return num + (10 - num % 10);
+        if (num % 10 < 5) return num - num % 10;
         return num;
     }
 
@@ -40,18 +46,18 @@ public class Logic2 {
     }
 
     static int luckySum(int a, int b, int c) {
-        if(a == 13) return 0;
-        if(b == 13) return a;
-        if(c == 13) return a + b;
+        if (a == 13) return 0;
+        if (b == 13) return a;
+        if (c == 13) return a + b;
         return a + b + c;
     }
 
     static int loneSum(int a, int b, int c) {
-       if(a == b && a == c) return 0;
-       if(a == b) return c;
-       if(a == c) return b;
-       if(b == c) return a;
-       return a + b + c;
+        if (a == b && a == c) return 0;
+        if (a == b) return c;
+        if (a == c) return b;
+        if (b == c) return a;
+        return a + b + c;
     }
 
     static boolean makeBricks(int small, int big, int goal) {
