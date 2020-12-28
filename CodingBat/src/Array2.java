ac9a2f0c5cd3bf01267a892cebc94d9ba2db7f6f
Yaroslav Kovalsky <yatsektest@gmail.com>
2020-12-28 14:32:15 +0300

    countEvens is solved

--- CodingBat/src/Array2.java
+++ CodingBat/src/Array2.java
@@ -2,4 +2,11 @@ public class Array2 {
     public static void main(String[] args) {
 
     }
+
+    static int countEvens(int[] nums) {
+        int count = 0;
+        for (int num : nums) if (num % 2 == 0) count++;
+        return count;
+    }
+
 }
