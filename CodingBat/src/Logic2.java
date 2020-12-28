5b4d368576f17fc2364458f62c4276cf7d754ed1
Yaroslav Kovalsky <yatsektest@gmail.com>
2020-12-28 10:32:51 +0300

    luckySum is solved

--- CodingBat/src/Logic2.java
+++ CodingBat/src/Logic2.java
@@ -3,6 +3,13 @@ public class Logic2 {
 
     }
 
+    static int luckySum(int a, int b, int c) {
+        if(a == 13) return 0;
+        if(b == 13) return a;
+        if(c == 13) return a + b;
+        return a + b + c;
+    }
+
     static int loneSum(int a, int b, int c) {
        if(a == b && a == c) return 0;
        if(a == b) return c;
