87053305343fa6e38297165c906889c64f86373c
Yaroslav Kovalsky <yatsektest@gmail.com>
2020-12-28 11:00:53 +0300

    roundSum is solved

--- CodingBat/src/Logic2.java
+++ CodingBat/src/Logic2.java
@@ -3,6 +3,16 @@ public class Logic2 {
 
     }
 
+    static int roundSum(int a, int b, int c) {
+       return round10(a) + round10(b) + round10(c);
+    }
+
+    static int round10(int num) {
+        if(num % 10 >= 5) return num + (10 - num% 10);
+        if(num % 10 < 5) return num - num % 10;
+        return num;
+    }
+
     static int noTeenSum(int a, int b, int c) {
         return fixTeen(a) + fixTeen(b) + fixTeen(c);
     }
