d3ed071f411e929b641e449e830057a54b853e4d
Yaroslav Kovalsky <yatsektest@gmail.com>
2021-01-07 20:15:36 +0300

    matchUp is solved

--- CodingBat/src/Array2.java
+++ CodingBat/src/Array2.java
@@ -5,6 +5,14 @@ public class Array2 {
 
     }
 
+    public int matchUp(int[] nums1, int[] nums2) {
+        int count = 0;
+        for (int i = 0; i < nums1.length; i++) {
+            if(Math.abs(nums1[i] - nums2[i]) <= 2 && nums1[i] != nums2[i]) count++;
+        }
+        return count;
+    }
+
     static boolean either24(int[] nums) {
         boolean is22 = false;
         boolean is44 = false;
