dd27413da15b5ee10d613abbb116a29bff7037cd
Yaroslav Kovalsky <yatsektest@gmail.com>
2021-01-08 20:50:56 +0300

    twoTwo is solved with an internet help

--- CodingBat/src/Array2.java
+++ CodingBat/src/Array2.java
@@ -5,6 +5,16 @@ public class Array2 {
 
     }
 
+    static boolean twoTwo(int[] nums) {
+        if (nums.length == 1 && nums[0] == 2) return false;
+        if (nums.length >= 2 && ((nums[0] == 2 && nums[1] != 2) ||
+                (nums[nums.length - 1] == 2 && nums[nums.length - 2] != 2))) return false;
+        for (int i = 1; i < nums.length - 1; i++) {
+            if (nums[i] == 2 && nums[i - 1] != 2 && nums[i + 1] != 2) return false;
+        }
+        return true;
+    }
+
     static boolean haveThree(int[] nums) {
         int countThree = 0;
         boolean threeNot = true;
