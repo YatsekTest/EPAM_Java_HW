1c2ceadec1392908862a6a5a99ff2847a6d3b2bc
Yaroslav Kovalsky <yatsektest@gmail.com>
2021-01-13 19:29:20 +0300

    right2 is solved

--- CodingBat/src/String1.java
+++ CodingBat/src/String1.java
@@ -4,6 +4,10 @@ public class String1 {
 
     }
 
+    static String right2(String str) {
+        return str.substring(str.length() - 2) + str.substring(0, str.length() - 2);
+    }
+
     static String left2(String str) {
         return str.substring(2) + str.substring(0, 2);
     }
