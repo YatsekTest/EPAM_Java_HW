bf0b6f2cdbf1d432fbc3d1b9b031a445a8c5814f
Yaroslav Kovalsky <yatsektest@gmail.com>
2021-01-13 19:03:33 +0300

    makeOutWord is solved

--- CodingBat/src/String1.java
+++ CodingBat/src/String1.java
@@ -3,6 +3,10 @@ public class String1 {
 
     }
 
+    static String makeOutWord(String out, String word) {
+        return out.substring(0, 2) + word + out.substring(2, 4);
+    }
+
     static String makeTags(String tag, String word) {
         return String.format("<%s>%s</%s>", tag, word, tag);
     }
