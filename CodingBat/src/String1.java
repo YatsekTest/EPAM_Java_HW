d4ecb4f314dcfe3e0d9715b6a89152b57c3f7181
Yaroslav Kovalsky <yatsektest@gmail.com>
2021-01-13 21:18:28 +0300

    nTwice is solved

--- CodingBat/src/String1.java
+++ CodingBat/src/String1.java
@@ -4,6 +4,10 @@ public class String1 {
 
     }
 
+    static String nTwice(String str, int n) {
+        return str.substring(0, n) + str.substring(str.length() - n);
+    }
+
     static boolean endsLy(String str) {
         if (str.length() >= 2) return str.endsWith("ly");
         else return false;
