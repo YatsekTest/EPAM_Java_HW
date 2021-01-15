77671280a596241c74a5109ee95c8b4f170e8f0f
Yaroslav Kovalsky <yatsektest@gmail.com>
2021-01-15 18:51:00 +0300

    withoutX2 is solved

--- CodingBat/src/String1.java
+++ CodingBat/src/String1.java
@@ -4,6 +4,14 @@ public class String1 {
 
     }
 
+    static String withoutX2(String str) {
+        StringBuilder newStr = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            if (!((i == 0 || i == 1) && str.charAt(i) == 'x')) newStr.append(str.charAt(i));
+        }
+        return newStr.toString();
+    }
+
     static String withoutX(String str) {
         StringBuilder newStr = new StringBuilder();
         for (int i = 0; i < str.length(); i++) {
