903cde809432603e4d9347eb6919cb5977b7eeb3
Yaroslav Kovalsky <yatsektest@gmail.com>
2021-01-16 11:29:17 +0300

    bobThere id solved

--- CodingBat/src/String2.java
+++ CodingBat/src/String2.java
@@ -3,6 +3,14 @@ public class String2 {
 
     }
 
+    static boolean bobThere(String str) {
+        if (str.length() < 3) return false;
+        for (int i = 0; i < str.length() - 2; i++) {
+            if (str.charAt(i) == 'b' && str.charAt(i + 2) == 'b') return true;
+        }
+        return false;
+    }
+
     static boolean xyzThere(String str) {
         if (str.length() < 3) return false;
         if (str.startsWith("xyz")) return true;
