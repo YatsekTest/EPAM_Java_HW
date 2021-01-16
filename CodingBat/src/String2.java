4aef5a2cc26313d8aa672176f234be62e5c6d5a8
Yaroslav Kovalsky <yatsektest@gmail.com>
2021-01-16 10:36:30 +0300

    doubleChar is solved

--- CodingBat/src/String2.java
+++ CodingBat/src/String2.java
@@ -2,4 +2,13 @@ public class String2 {
     public static void main(String[] args) {
 
     }
+
+    static String doubleChar(String str) {
+        StringBuilder newStr = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            newStr.append(str.charAt(i)).append(str.charAt(i));
+        }
+        return newStr.toString();
+    }
+
 }
