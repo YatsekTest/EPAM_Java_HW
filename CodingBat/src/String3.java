58ce86bba1f7420b08a5e3e47721ec2e7ec72917
Yaroslav Kovalsky <yatsektest@gmail.com>
2021-01-22 16:36:13 +0300

    countYZ is solved

--- CodingBat/src/String3.java
+++ CodingBat/src/String3.java
@@ -2,4 +2,17 @@ public class String3 {
     public static void main(String[] args) {
 
     }
+
+    public int countYZ(String str) {
+        int count = 0;
+        for (int i = 1; i < str.length(); i++) {
+            char ch = Character.toLowerCase(str.charAt(i - 1));
+            if (!Character.isLetter(str.charAt(i)) && (ch == 'y'
+                    || ch == 'z')) count++;
+            if (i == str.length() - 1 && (Character.toLowerCase(str.charAt(i)) == 'y'
+                    || Character.toLowerCase(str.charAt(i)) == 'z')) count++;
+        }
+        return count;
+    }
+
 }
