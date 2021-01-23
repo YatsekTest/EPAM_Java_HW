7d4c5f81b542a02375d8a9301657735f049c452b
Yaroslav Kovalsky <yatsektest@gmail.com>
2021-01-23 19:59:53 +0300

    gHappy is solved

--- CodingBat/src/String3.java
+++ CodingBat/src/String3.java
@@ -5,6 +5,18 @@ public class String3 {
 
     }
 
+    public boolean gHappy(String str) {
+        boolean isHappy = true;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'g') {
+                if (i > 0 && str.charAt(i - 1) == 'g') isHappy = true;
+                else isHappy = i < str.length() - 1 && str.charAt(i + 1) == 'g';
+            }
+        }
+        return isHappy;
+    }
+
+
     public boolean equalIsNot(String str) {
         int countIs = 0, countNot = 0;
         for (int i = 0; i < str.length(); i++) {
