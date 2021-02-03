8915c0c4fb70e7a2d37a4f138540d8c3743638cc
Yaroslav Kovalsky <yatsektest@gmail.com>
2021-02-03 14:41:38 +0300

    JavaStack is solved

--- HackerRank/src/JavaStack.java
+++ HackerRank/src/JavaStack.java
@@ -1,3 +1,40 @@
+import java.util.*;
+
 public class JavaStack {
 
+    public static void main(String[] argh) {
+        Scanner sc = new Scanner(System.in);
+        Stack<Character> stack = new Stack<>();
+
+        while (sc.hasNext()) {
+            String input = sc.next();
+            char[] chars = input.toCharArray();
+            for (char c : chars) {
+                if (c == '[' || c == '(' || c == '{') {
+                    stack.push(c);
+                    continue;
+                }
+                if (c == ']' && !stack.empty() && stack.peek() == '[') {
+                    stack.pop();
+                    continue;
+                }
+                if (c == ')' && !stack.empty() && stack.peek() == '(') {
+                    stack.pop();
+                    continue;
+                }
+                if (c == '}' && !stack.empty() && stack.peek() == '{') {
+                    stack.pop();
+                    continue;
+                }
+                if (c == ']' || c == ')' || c == '}') {
+                    stack.push(c);
+                    break;
+                }
+            }
+            System.out.println(stack.isEmpty());
+            stack.clear();
+        }
+
+    }
 }
+
