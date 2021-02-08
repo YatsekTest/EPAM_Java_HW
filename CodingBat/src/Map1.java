2b504eca49d2721971a68d4d2987879166436869
Yaroslav Kovalsky <yatsektest@gmail.com>
2021-02-08 16:07:08 +0300

    mapBully is solved

--- CodingBat/src/Map1.java
+++ CodingBat/src/Map1.java
@@ -1,8 +1,16 @@
+import java.util.Map;
+
 public class Map1 {
     public static void main(String[] args) {
 
     }
 
-
+    public Map<String, String> mapBully(Map<String, String> map) {
+        if (map.containsKey("a")) {
+            map.put("b", map.get("a"));
+            map.put("a", "");
+        }
+        return map;
+    }
 
 }
