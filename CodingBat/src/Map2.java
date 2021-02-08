4b595dceb746ca50f01ba608e32907971fb81667
Yaroslav Kovalsky <yatsektest@gmail.com>
2021-02-08 18:37:36 +0300

    word0 is solved

--- CodingBat/src/Map2.java
+++ CodingBat/src/Map2.java
@@ -1,8 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Map2 {
     public static void main(String[] args) {
 
     }
 
-
+    public Map<String, Integer> word0(String[] strings) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String str : strings) {
+            map.put(str, 0);
+        }
+        return map;
+    }
 
 }
