21e34522868086eb4106fdae1f143820f74a11b9
Yaroslav Kovalsky <yatsektest@gmail.com>
2021-02-08 20:04:44 +0300

    allSwap is solved with an internet help

--- CodingBat/src/Map2.java
+++ CodingBat/src/Map2.java
@@ -6,6 +6,21 @@ public class Map2 {
 
     }
 
+    public String[] allSwap(String[] strings) {
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < strings.length; i++) {
+            String key = String.valueOf(strings[i].charAt(0));
+            if (map.containsKey(key)) {
+                int pos = map.get(key);
+                String tmp = strings[pos];
+                strings[pos] = strings[i];
+                strings[i] = tmp;
+                map.remove(key);
+            } else map.put(key, i);
+        }
+        return strings;
+    }
+
     public Map<String, Boolean> wordMultiple(String[] strings) {
         Map<String, Boolean> map = new HashMap<>();
         for (String string : strings) {
