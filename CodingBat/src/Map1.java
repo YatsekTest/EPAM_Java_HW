e5fc38944fa6c066e74242233d593360238a9f84
Yaroslav Kovalsky <yatsektest@gmail.com>
2021-02-08 16:15:12 +0300

    mapAB is solved

--- CodingBat/src/Map1.java
+++ CodingBat/src/Map1.java
@@ -5,6 +5,14 @@ public class Map1 {
 
     }
 
+    public Map<String, String> mapAB(Map<String, String> map) {
+        if (map.containsKey("a") && map.containsKey("b")) {
+            map.put("ab", map.get("a") + map.get("b"));
+        }
+        return map;
+    }
+
+
     public Map<String, String> mapShare(Map<String, String> map) {
         if (map.containsKey("a")) map.put("b", map.get("a"));
         map.remove("c");
