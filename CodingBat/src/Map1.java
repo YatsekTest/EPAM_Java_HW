3df0dd392c53e828878054fe23e0263f791c5f35
Yaroslav Kovalsky <yatsektest@gmail.com>
2021-02-08 18:20:19 +0300

    mapAB4 is solved

--- CodingBat/src/Map1.java
+++ CodingBat/src/Map1.java
@@ -5,6 +5,18 @@ public class Map1 {
 
     }
 
+    public Map<String, String> mapAB4(Map<String, String> map) {
+        if (map.containsKey("a") && map.containsKey("b")) {
+            if (map.get("a").length() > map.get("b").length()) map.put("c", map.get("a"));
+            if (map.get("a").length() < map.get("b").length()) map.put("c", map.get("b"));
+            if (map.get("a").length() == map.get("b").length()) {
+                map.put("a", "");
+                map.put("b", "");
+            }
+        }
+        return map;
+    }
+
     public Map<String, String> mapAB3(Map<String, String> map) {
         if (map.containsKey("a") && !map.containsKey("b")) map.put("b", map.get("a"));
         if (map.containsKey("b") && !map.containsKey("a")) map.put("a", map.get("b"));
